e0344c6ef3f1da247fa639f078b70bec331e36e6
Susankemigisa <kemigisa32@gmail.com>
2025-09-16 16:13:59 +0300

    adds methhod overide functionality in server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 const expressSession = require("express-session")
 const MongoStore = require("connect-mongo");
 const moment = require("moment");
+const methodOverride = require("method-override");
 
 require('dotenv').config();
 const UserModel = require("./models/userModel")
@@ -41,6 +42,8 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 // 4. Middleware
+//method overide
+app.use(methodOverride("_method"));
 // app.use(express.static('public')); //static files
 app.use(express.static(path.join(__dirname, "public")))
 // app.use("/public/uploads", express.static(__dirname + "/public/uploads"))
